738d1e5a574ea2b2b9aacfd8b7120c6c80bd0ea9
Gómez <josue-salatiel.gomez@atos.net>
2022-02-14 13:48:22 -0600

    state modification on App.tsx

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
+// Types
+import { QuestionState, Difficulty } from './API';
 
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 const TOTAL_QUESTIONS = 10;
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumver] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
   const startTrivia = async () => {
 
   }
@@ -30,18 +40,17 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard
+      {/* <QuestionCard
       questionNr={number + 1}
       totalQuestions={TOTAL_QUESTIONS}
       question={questions[number].question}
       answer={questions[number].answers}
       userAnswer={userAnswers ? userAnswers[number] : undefined}
       callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
-    Quiz
     </div>
   );
 }
