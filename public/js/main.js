e2d8b3b57cd4a7bbb53c55018290e4d7d821456b
Gómez <josue-salatiel.gomez@atos.net>
2022-02-17 15:58:08 -0600

    Compared with github to add the last modifications to download a csv

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
+const issuesOutput = document.querySelector('#issues')
+const issuesCount = document.querySelector('#number')
+const alertMessage= '<div class="alert alert-danger" role="alert">Something went wrong</div>'
+const emptyUrl= '<div class="alert alert-danger" role="alert">Please add an URL</div>'
+const warningMessage= '<div class="alert alert-warning" role="alert">no Issues Found</div>'
+const CsvMessage= '<div class="alert alert-warning" role="alert">CSV not available</div>'
+
+
 // Fetch accessibility issues
-const testAccesability = async (e) => {
+const testAccessibility = async (e) => {
     e.preventDefault()
-
     const url = document.querySelector('#url').value
-
     if(url === '') {
-        alert('Please add a url')
+        issuesOutput.innerHTML = emptyUrl
     } else {
         setLoading()
 
@@ -13,23 +19,58 @@ const testAccesability = async (e) => {
 
         if(response.status !== 200) {
             setLoading(false)
-            alert('Something went wrong')
+            issuesOutput.innerHTML = alertMessage
         } else {
             const { issues } = await response.json()
             addIssuesToDOM(issues)
             setLoading(false)
+            document.getElementById("clearResults").classList.remove("hideButton")
+            document.getElementById("csvBtn").classList.remove("hideButton")
         }
     }
 }
+
+// Download CSV
+const csvIssues = async (e) => {
+    e.preventDefault()
+    const url = document.querySelector('#url').value
+    if(url === '') {
+        issuesOutput.innerHTML = emptyUrl
+    } else {
+        const response = await fetch(`/api/test?url=${url}`)
+
+        if(response.status !== 200) {
+            setLoading(false)
+            alert(CsvMessage)
+        } else if (issues.length === 0) {
+            alert(CsvMessage)
+        } else {
+            const { issues } = await response.json()
+            const csv = issues.map(issue => { return `${issue.code},${issue.message},${issue.context}`}).join('\n')
+                
+            const csvBlob = new Blob([csv], { type: 'text/csv' })
+            const csvUrl = URL.createObjectURL(csvBlob)
+            const link = document.createElement('a')
+            link.href =csvUrl
+            link.download = 'Accessibility_issues_list_'+url.substrin(12)+'.csv'
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link)
+        }
+    }
+}
+
 // Add issues to DOM
 const addIssuesToDOM = (issues) => {
-    const issuesOutput = document.querySelector('#issues')
-
     issuesOutput.innerHTML = ''
+    issuesCount.innerHTML = ''
 
     if(issues.length === 0) {
-        issuesOutput.innerHTML = '<h4> No Issues Found</h4>'
+        issuesOutput.innerHTML = warningMessage
     } else {
+        issuesCount.innerHTML = `
+      <p class="alert alert-warning">${issues.length} issues found !</p>
+    `
         issues.forEach((issue) => {
             const output = `
             <div class="card mb-5>
@@ -44,7 +85,7 @@ const addIssuesToDOM = (issues) => {
             </div>
             `
             issuesOutput.innerHTML += output
-        });
+        })
     }
 }
 
@@ -53,6 +94,7 @@ const setLoading = (isLoading = true) => {
     const loader = document.querySelector('.loader')
     if(isLoading) {
         loader.style.display = 'block'
+        issuesOutput.innerHTML = ''
     } else {
         loader.style.display = 'none'
     }
@@ -61,11 +103,21 @@ const setLoading = (isLoading = true) => {
 // Escape HTML
 function escapeHTML(html) {
     return html
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#039;')
-  }
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;')
+}
 
-document.querySelector('#form').addEventListener('submit',testAccesability)
+// Clear results
+const clearResults = (e) => {
+    e.preventDefault()
+    issuesOutput.innerHTML = ''
+    issuesCount.innerHTML = ''
+    document.querySelector('#url').value = ''
+}
+
+document.querySelector('#form').addEventListener('submit', testAccessibility)
+document.querySelector('#clearResults').addEventListener('click', clearResults)
+document.querySelector('#csvBtn').addEventListener('click', csvIssues)
