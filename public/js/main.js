a2351b6ca5d972a89019075542802c9d608761da
Gómez <josue-salatiel.gomez@atos.net>
2022-02-17 15:27:18 -0600

    Add Issues To DOM

--- public/js/main.js
+++ public/js/main.js
@@ -15,13 +15,38 @@ const testAccesability = async (e) => {
             setLoading(false)
             alert('Something went wrong')
         } else {
-            const data = await response.json()
+            const { issues } = await response.json()
             addIssuesToDOM(issues)
             setLoading(false)
         }
     }
 }
 // Add issues to DOM
+const addIssuesToDOM = (issues) => {
+    const issuesOutput = document.querySelector('#issues')
+
+    issuesOutput.innerHTML = ''
+
+    if(issues.length === 0) {
+        issuesOutput.innerHTML = '<h4> No Issues Found</h4>'
+    } else {
+        issues.forEach((issue) => {
+            const output = `
+            <div class="card mb-5>
+                <div class="card-body">
+                <h4>${issue.message}</h4>
+                <p class="bg-light p-3 my-3">
+                    ${escapeHTML(issue.context)}
+                </p>
+                <p class="bg-secondary text-light p-2">
+                    CODE: ${issue.code}
+                </p>
+            </div>
+            `
+            issuesOutput.innerHTML += output
+        });
+    }
+}
 
 // Set loading state
 const setLoading = (isLoading = true) => {
@@ -32,6 +57,15 @@ const setLoading = (isLoading = true) => {
         loader.style.display = 'none'
     }
 }
+
 // Escape HTML
+function escapeHTML(html) {
+    return html
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;')
+  }
 
 document.querySelector('#form').addEventListener('submit',testAccesability)
