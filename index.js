5fa5e05a873caaabffc7dcc9be26263cd834c71f
Gómez <josue-salatiel.gomez@atos.net>
2022-02-18 16:14:26 -0600

    yarn crash course

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const express = require('express')
-const pa11y = require('pa11y')
-const PORT = process.env.PORT || 5000
+// console.log("Hello WebstylePress - from index")
 
-const app = express()
-
-app.use(express.static('public'))
-
-app.get('/api/test', async (req,res) => {
-    if(!req.query.url) {
-        res.status(400).json({error: 'url is required'})
-    } else {
-        const results = await pa11y(req.query.url)
-        res.status(200).json(results)
-    }
-})
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const moment = require('moment')
+console.log( moment().format('MMMM Do YYYY HH:mm'));
